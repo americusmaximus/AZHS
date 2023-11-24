f036a7e6d6a057f51140ec447952f9f7aebf1279
Americus Maximus <americancodemax@gmail.com>
2023-11-24 18:38:04 -0500

    Minor improvement.

--- Source/R.DirectX.6.0.3DS/Module.cxx
+++ Source/R.DirectX.6.0.3DS/Module.cxx
@@ -579,6 +579,8 @@ namespace RendererModule
         }
         case RENDERER_MODULE_STATE_SELECT_SHADE_STATE:
         {
+            // NOTE: Original code does not have this if statement.
+            // The positive if comparison always executes in the original code.
             if (SettingsState.FlatShading)
             {
                 if ((u32)value == RENDERER_MODULE_SHADE_FLAT)
